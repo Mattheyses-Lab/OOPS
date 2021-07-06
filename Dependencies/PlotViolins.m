35acfaecc450451bab40449b5feb055e05e26178
will-f-dean-36 <will.f.dean.36@gmail.com>
2021-07-06 17:01:11 -0500

    Minor updates, will soon offer more complex functionality for plotting across groups

--- Dependencies/PlotViolins.m
+++ Dependencies/PlotViolins.m
@@ -1,47 +1,57 @@
 function PlotViolins(source,event)
-    PODSData = guidata(source);
-    
+
+    PODSData = guidata(source);   
+
     nGroups = PODSData.nGroups;
     
-    xdata = [];
-    ydata = [];
+    x = [];
+    y = [];
     
+    ss = PODSData.Settings.ScreenSize;
+    % center point (x,y) of screen
+    center = [ss(3)/2,ss(4)/2];
     
-    % for each group
-    for i = 1:nGroups
-        % current group
-        cGroup = PODSData.Group(i);
-        % n images in group
-        nImages = cGroup.nReplicates;
-        % for each image in group
-        for j = 1:nImages
-            % current replicate
-            cReplicate = cGroup.Replicate(j);
+    sz = [center(1)-500 center(2)-500 1000 1000];   
+    
+    switch source.Text
+        case 'Violin'
             
-            ydata = [ydata,cReplicate.Object.OFAvg];
-        end
-        % start index
-        idx1 = size(xdata,2)+1;
-        % end index
-        idx2 = size(ydata,2);
-        
-        
-        for ii = idx1:idx2
-            xdata{ii} = cGroup.GroupName;
-        end
+            % for each group
+            for i = 1:nGroups
+                % current group
+                cGroup = PODSData.Group(i);
+                % n images in group
+                nImages = cGroup.nReplicates;
+                % for each image in group
+                for j = 1:nImages
+                    % current replicate
+                    cReplicate = cGroup.Replicate(j);
 
-    end
-    
-    ydata = ydata';
-    xdata = xdata';
-    
-    
-    figure(2);
-    
-    
-    violinplt = violinplot(ydata,xdata);
-    
-    
-    
+                    y = [y,cReplicate.Object.OFAvg];
+                end
+                % start index
+                idx1 = size(x,2)+1;
+                % end index
+                idx2 = size(y,2);
 
-end
+
+                for ii = idx1:idx2
+                    x{ii} = cGroup.GroupName;
+                end
+
+            end
+
+            y = y';
+            x = x';
+
+
+            figure(2);
+
+
+            violinplt = violinplot(y,x);
+            
+
+
+    end % end of main switch block  
+
+end % end function
