6b96d59f9f1e0a3133db9287ac76ff4e4755ec4a
will-f-dean-36 <will.f.dean.36@gmail.com>
2021-10-01 10:44:16 -0500

    Takes linear subimage indices, SubarrayIdxIn, and integer, Padding, returns set of subimage indices padded on all sides with n px = Padding

---
+++ Dependencies/padSubarrayIdx.m
@@ -0,0 +1,45 @@
+function SubarrayIdxOut = padSubarrayIdx(SubarrayIdxIn,Padding)
+
+    X = SubarrayIdxIn{1,1};
+    Y = SubarrayIdxIn{1,2};
+    
+    % linearly extend X and Y subarrays by 5 elements in both directions
+    XMax = X(end)+Padding;
+    XMin = X(1)-Padding;
+    NewX = [XMin:1:XMax];
+    
+    YMax = Y(end)+Padding;
+    YMin = Y(1)-Padding;
+    NewY = [YMin:1:YMax];
+    clear YMax YMin XMax XMin
+    
+    
+    if mod(length(NewX),2) % if odd # of elements
+        NewX(end+1) = NewX(end) + 1; % then make even by adding element
+    end
+    
+    if mod(length(NewY),2) % if odd # of elements
+        NewY(end+1) = NewY(end) + 1; % then make even by adding element
+    end
+ 
+    if length(NewX) > length(NewY) % extend Y to match length of X
+
+        delta = (length(NewX) - length(NewY)) / 2;
+        YMax = NewY(end)+delta;
+        YMin = NewY(1)-delta;
+        clear NewY
+        NewY = [YMin:1:YMax];
+        
+    elseif length(NewX) < length(NewY) % extend X to match length of Y
+
+        delta = (length(NewY) - length(NewX)) / 2;
+        XMax = NewX(end)+delta;
+        XMin = NewX(1)-delta;
+        clear NewX
+        NewX = [XMin:1:XMax];        
+        
+    end
+
+    SubarrayIdxOut = {NewX,NewY};
+
+end
