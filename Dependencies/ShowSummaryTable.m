575a9bf8c5965b7979ff688fd082a32d5cd16324
will-f-dean-36 <will.f.dean.36@gmail.com>
2021-10-01 10:51:31 -0500

    Show summary table of all objects. Need to add more interactivity (sorting, filtering, etc.)

--- Dependencies/ShowSummaryTable.m
+++ Dependencies/ShowSummaryTable.m
@@ -1,13 +1,18 @@
 function ShowSummaryTable(source,event)
 
+    % get the summary table
     T = SavePODSData(source);
+    
+    % get handles
     PODSData = guidata(source);
 
+    % new figure to show summary table
     SummaryFig = uifigure('Position',PODSData.Handles.fH.Position);
     
+    % uitable to hold data
     uit = uitable(SummaryFig,'data',T);
     
+    % maximize table size within figure window
     uit.Position = PODSData.Handles.fH.InnerPosition;
-
 
 end
