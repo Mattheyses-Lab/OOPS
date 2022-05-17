14c1824ab307d4ef783646afe11670f630da1070
will-f-dean-36 <will.f.dean.36@gmail.com>
2022-05-17 11:56:01 -0500

    Helper function to scale images from a range of 0 to 1

---
+++ Dependencies/Scale0To1.m
@@ -0,0 +1,28 @@
+function Scaled = Scale0To1(UnscaledImage)
+
+    sz = size(UnscaledImage);
+    ndim = length(sz);
+    
+    if ndim > 3
+        error('Dimensions > 3 not supported!');
+        return
+    end
+    
+    switch ndim
+        
+        case 3
+            max_value = max(max(max(UnscaledImage)));
+            min_value = min(min(min(UnscaledImage)));
+        case 2
+            max_value = max(max(UnscaledImage));
+            min_value = min(min(UnscaledImage));            
+        case 1
+            max_value = max(UnscaledImage);
+            min_value = min(UnscaledImage); 
+    end
+
+    Scaled = zeros(sz);
+
+    Scaled = (UnscaledImage-min_value)./(max_value-min_value);
+
+end
