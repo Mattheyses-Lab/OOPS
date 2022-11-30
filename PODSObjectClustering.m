3dc2748c38a88d581c60b06a791fbde531c29a65
will-f-dean-36 <will.f.dean.36@gmail.com>
2022-11-30 12:38:56 -0600

    Improved control over k-means clustering

--- PODSObjectClustering.m
+++ PODSObjectClustering.m
@@ -1,4 +1,10 @@
-function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,VariablesList,VariablesToPlot)
+function [ClusterIdxs,nClusters] = PODSObjectClustering(ObjectData,...
+    nClusters,...
+    nReplicates,...
+    VariablesList,...
+    VariablesToPlot,...
+    nClustersMode,...
+    Criterion)
 
     Var1Name = VariablesList{VariablesToPlot(1)};
     Var2Name = VariablesList{VariablesToPlot(2)};
@@ -18,7 +24,7 @@ function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,Var
 %     [silh2,h] = silhouette(ObjectData,cidx2,'sqeuclidean');
 %     
     % different symbols for each cluster
-    ptsymb = {'bs','r^','go','md','c+'};
+    %ptsymb = {'bs','r^','go','md','c+','ysquare'};
     
 %     figure('Name','3D Scatter (Euclidean Distance)')
 %     
@@ -50,8 +56,36 @@ function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,Var
 %     % display average silhouette width
 %     disp(['Mean Silhouette Width (Euclidean Distance): ',num2str(mean(silh2))])
 
-%% Euclidean distance k-means clustering (with more replicates)    
-    
+%% Euclidean distance k-means clustering (DETERMINE OPTIMUM NUMBER OF CLUSTERS, with more replicates) BEST SO FAR
+
+switch nClustersMode
+    case 'Manual'
+        disp(['Performing k-means clustering with ',num2str(nClusters),' clusters...']);
+    case 'Auto'
+        disp(['Determining the optimal value of k (max=10) with the ',Criterion,' criterion...']);
+        clust = zeros(size(ObjectData,1),10);
+        % sums of squared errors for each object - needs work
+        %Distances = zeros(size(ObjectData,1),10);
+        Sums = zeros(1,10);
+        for i=1:10
+%             [clust(:,i),~,~,D] = kmeans(ObjectData,i,'replicate',nReplicates);
+%             Distances(:,i) = D(:,i);
+            [clust(:,i),~,S,~] = kmeans(ObjectData,i,'replicate',nReplicates);
+            Sums(1,i) = sum(S);
+        end
+        % within-cluster-sum of squared errors (WSS)
+        % (sum of squared distances between all points and their centroids)
+%         WSS = sum(Distances.^2);
+        figure('Name','Within-Cluster Sums');
+        plot(1:10,Sums);
+        xlabel('Number of clusters (k)');
+        ylabel('Sum of within-cluster point-to-centroid distances');
+
+        ClusterEvalObj = evalclusters(ObjectData,clust,Criterion);
+        nClusters = ClusterEvalObj.OptimalK;
+        disp(['Optimal number of clusters: ',num2str(nClusters)]);
+end
+
     % use 'replicates' name-value pair to repeat the clustering algorithm
     %   for different randomly selected centroids for each replicate 
     [cidx3,cmeans3,sumd3] = kmeans(ObjectData,nClusters,'replicates',nReplicates,'display','final');
@@ -64,7 +98,6 @@ function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,Var
     [silh3,h] = silhouette(ObjectData,cidx3,'sqeuclidean');
     % not quite as well separated as when we used 2 clusters
     
-    
     figure('Name',['3D Scatter (Euclidean Distance, ',num2str(nReplicates),' replicates)'])
     % can plot the raw data again to see how kmeans assigned points to clusters
     %   -> upper cluster split into 2 smaller clusters
@@ -73,7 +106,7 @@ function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,Var
         plot3(ObjectData(clust,VariablesToPlot(1)),...
             ObjectData(clust,VariablesToPlot(2)),...
             ObjectData(clust,VariablesToPlot(3)),...
-            ptsymb{i});
+            'o');
         hold on
     end
     
@@ -93,6 +126,49 @@ function ClusterIdxs = PODSObjectClustering(ObjectData,nClusters,nReplicates,Var
 
     % display average silhouette width
     disp(['Mean Silhouette Width (Euclidean Distance, ',num2str(nReplicates),' replicates): ',num2str(mean(silh3))])
+
+%% Euclidean distance k-means clustering (user-defined nclusters, with more replicates) BEST SO FAR
+    
+%     % use 'replicates' name-value pair to repeat the clustering algorithm
+%     %   for different randomly selected centroids for each replicate 
+%     [cidx3,cmeans3,sumd3] = kmeans(ObjectData,nClusters,'replicates',nReplicates,'display','final');
+%     ClusterIdxs = cidx3;
+%     % 3rd output, sumd3, contains sum of distance within each cluster for the best solution
+% 
+%     figure('Name',['Silhouette (Euclidean Distance,',num2str(nReplicates),' replicates)'])
+%     
+%     % again use silhouette plot to visualize cluster separation
+%     [silh3,h] = silhouette(ObjectData,cidx3,'sqeuclidean');
+%     % not quite as well separated as when we used 2 clusters
+%     
+%     figure('Name',['3D Scatter (Euclidean Distance, ',num2str(nReplicates),' replicates)'])
+%     % can plot the raw data again to see how kmeans assigned points to clusters
+%     %   -> upper cluster split into 2 smaller clusters
+%     for i = 1:nClusters
+%         clust = find(cidx3==i);
+%         plot3(ObjectData(clust,VariablesToPlot(1)),...
+%             ObjectData(clust,VariablesToPlot(2)),...
+%             ObjectData(clust,VariablesToPlot(3)),...
+%             ptsymb{i});
+%         hold on
+%     end
+%     
+%     % cluster centroids are plotted with circled x's
+%     plot3(cmeans3(:,VariablesToPlot(1)),cmeans3(:,VariablesToPlot(2)),cmeans3(:,VariablesToPlot(3)),'ko');
+%     plot3(cmeans3(:,VariablesToPlot(1)),cmeans3(:,VariablesToPlot(2)),cmeans3(:,VariablesToPlot(3)),'kx');
+%     hold off
+%     
+%     % add axes labels
+%     xlabel(Var1Name);
+%     ylabel(Var2Name);
+%     zlabel(Var3Name);
+%     
+%     % set viewing angle
+%     view(-137,10);
+%     grid on
+% 
+%     % display average silhouette width
+%     disp(['Mean Silhouette Width (Euclidean Distance, ',num2str(nReplicates),' replicates): ',num2str(mean(silh3))])
     %[mean(silh2) mean(silh3)]
 
 %% Cosine distance k-means clustering
