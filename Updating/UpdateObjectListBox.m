f48e5baf0cc04b67de4223fb81ba24bf76989998
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-03-07 13:20:00 -0600

    cleaner logic

--- Updating/UpdateObjectListBox.m
+++ Updating/UpdateObjectListBox.m
@@ -18,25 +18,14 @@ function UpdateObjectListBox(source)
             % enable/update object selection
             PODSData.Handles.ObjectSelector.Enable = 1;
             PODSData.Handles.ObjectSelector.Items = Replicate.ObjectNames;
-            PODSData.Handles.ObjectSelector.ItemsData = 1:length(Replicate.ObjectNames);
+            PODSData.Handles.ObjectSelector.ItemsData = 1:Replicate.nObjects;
             PODSData.Handles.ObjectSelector.Value = Replicate.CurrentObjectIdx;
             scroll(PODSData.Handles.ObjectSelector,PODSData.Handles.ObjectSelector.Value);
             return
-%         else
-%             PODSData.Handles.ObjectSelector.Items = {'No objects found...'};
         end
-        % else, if there are no currently selected images
-%     elseif isempty(Replicate)
-%         % disable object selection listbox, instruct user to select image
-%         PODSData.Handles.ObjectSelector.Items = {'Select an image...'};
-%         PODSData.Handles.ObjectSelector.Enable = 0;
-%         % else, if the number of currently selected images > 1
-%     else
-%         % disable object selection listbox and indicate that too many images are selected
-%         PODSData.Handles.ObjectSelector.Items = {'Multiple images selected...'};
-%         PODSData.Handles.ObjectSelector.Enable = 0;
     end
 
+    % if the above conditions are unmet, clear and disable the listbox
     PODSData.Handles.ObjectSelector.Items = {};
     PODSData.Handles.ObjectSelector.Enable = 0;
 
