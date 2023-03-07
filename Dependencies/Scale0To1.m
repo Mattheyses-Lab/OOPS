7290d5c5f8fdeea066f5b39cec42e41cc53a6953
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-03-07 13:04:29 -0600

    Using MATLAB's rescale() function for more stable behavior

--- Dependencies/Scale0To1.m
+++ Dependencies/Scale0To1.m
@@ -1,5 +1,4 @@
 function Scaled = Scale0To1(UnscaledImage)
-% rescale the intensity imagevalues  to fall between 0 and 1
+% rescale the intensity image values to fall between 0 and 1
     Scaled = rescale(UnscaledImage,0,1);
-
 end
