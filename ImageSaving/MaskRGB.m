a1df0773bd6b4278d242eb81766d35698f60ec88
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-03-07 13:54:45 -0600

    Added commented example of how to show intensity-masked RGB on a white BG instead of black

--- ImageSaving/MaskRGB.m
+++ ImageSaving/MaskRGB.m
@@ -1,5 +1,11 @@
 function MaskedRGBImage = MaskRGB(UnmaskedRGBImage,Mask)
 
+    % RGB image masked with intensity image, Mask (black BG)
     MaskedRGBImage = bsxfun(@times, UnmaskedRGBImage, cast(Mask, 'like', UnmaskedRGBImage));
+
+    % uncommenting below will display the data on a white BG instead (leave above uncommented)
+    % WhiteRGBImage = ones(size(UnmaskedRGBImage), 'like', UnmaskedRGBImage);
+    % WhiteRGBImageMasked = bsxfun(@times, WhiteRGBImage, cast(imcomplement(Mask), 'like', WhiteRGBImage));
+    % MaskedRGBImage = MaskedRGBImage+WhiteRGBImageMasked;
 
 end
