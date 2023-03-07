b4e79be51c9bb5eab3fc8188a65f5a63e55e335d
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-03-07 13:07:19 -0600

    slight readability edits

--- Dependencies/labelBranches.m
+++ Dependencies/labelBranches.m
@@ -2,49 +2,24 @@ function [skelL,dilatedL] = labelBranches(I)
 % skeletonizes, then labels individual branches in skeletonized 
 % binary image and returns unskeletonized label matrix
 
+% skeletonized binary image
 y = bwskel(I);
 
 %% code below from https://dsp.stackexchange.com/questions/7622/find-branches-in-skeleton-image-using-matlab
+% has been edited
 
 mn=bwmorph(y,'branchpoints');
-[row column] = find(mn);
-branchPts    = [row column];
-endImg    = bwmorph(y, 'endpoints');
-[row column] = find(endImg);
-endPts       = [row column];
-% figure;imshow(y);
-% hold on ; 
-% plot(branchPts(:,2),branchPts(:,1),'rx');
-% hold on; plot(endPts(:,2),endPts(:,1),'*');
+% [row column] = find(mn);
+% branchPts    = [row column];
+% endImg    = bwmorph(y, 'endpoints');
+% [row column] = find(endImg);
+% endPts       = [row column];
+
+
 % Labeling the Branches
 branches = imsubtract(y,mn); % set branch points to zero
 % figure; imshow(branches);title('no bp')
 branchesLabeled = bwlabel(branches); % label connected components
-
-% WD edits below
-% figure();imshow(label2rgb(branchesLabeled));
-% end WD edits
-% WD: line below does not work
-%vislabels(branchesLabeled);
-
-% Calculation of Length of Branches and Circular Neighbourhood Method for
-% for detection of normal and abnormal branches.
-% sts = regionprops( branchesLabeled,'Area', 'Perimeter','MajorAxisLength','Centroid');
-% 
-% 
-% figure
-% imshow(branchesLabeled)
-% hold on
-% % Loop for circles
-% for i=1:size(sts)
-% r= sts(i).MajorAxisLength/2 ; %desired radius
-% centerx = sts(i).Centroid(1);  
-% centery = sts(i).Centroid(2);
-% th = 0:pi/50:2*pi;
-% xunit = r * cos(th) + centerx;
-% yunit = r * sin(th) + centery;
-% plot(xunit, yunit);
-% end
 
 %% end code from stack exchange
 
@@ -79,8 +54,8 @@ for i = 1:size(props)
     dilatedL(props(i).PixelIdxList) = temp_dilatedL(props(i).PixelIdxList);
 end
 
-dilatedLRGB = label2rgb(dilatedL);
 
+%dilatedLRGB = label2rgb(dilatedL);
 %figure();imshow(dilatedLRGB);
 
 
@@ -122,35 +97,44 @@ end
 
 function out = replacenansinmask(in,mask)
 
-u = in;
+out = in;
+
+inSize = size(in);
 
 % Now find the nan's
-nanLocations = isnan(u) & mask;
+nanLocations = isnan(in) & mask;
 nanLinearIndexes = find(nanLocations);
 
 % and non-nans (only inside the mask)
-nonNanLocations = ~isnan(u) & mask;
+nonNanLocations = ~isnan(in) & mask;
 nonNanLinearIndexes = find(nonNanLocations);
 
-%nonNanLinearIndexes = setdiff(1:numel(u), nanLinearIndexes);
-% Get the x,y,z of all other locations that are non nan.
-[xGood, yGood, zGood] = ind2sub(size(u), nonNanLinearIndexes);
+% Get the row and column locations of non-nan elements
+[R, C] = ind2sub(inSize, nonNanLinearIndexes);
+
 for index = 1 : length(nanLinearIndexes)
-  thisLinearIndex = nanLinearIndexes(index);
-  % Get the x,y,z location
-  % These are not actually x,y, and z coordinates in the mathematical sense!!!
-  [x,y,z] = ind2sub(size(u), thisLinearIndex);
-  % Get distances of this location to all the other locations
-  distances = sqrt((x-xGood).^2 + (y - yGood) .^ 2 + (z - zGood) .^ 2);
-  [sortedDistances, sortedIndexes] = sort(distances, 'ascend');
-  % The closest non-nan value will be located at index sortedIndexes(1)
-  indexOfClosest = sortedIndexes(1);
-  % Get the u value there.
-  goodValue = u(xGood(indexOfClosest), yGood(indexOfClosest), zGood(indexOfClosest));
-  % Replace the bad nan value in u with the good value.
-  u(x,y,z) = goodValue;
+    thisLinearIndex = nanLinearIndexes(index);
+    % convert linear idx to row and col coordinates
+    [r,c] = ind2sub(inSize, thisLinearIndex);
+    % Get distances of this location to all the other locations
+    distances = sqrt((r-R).^2 + (c-C).^ 2);
+    % ascending sort
+    %[~, sortedIndexes] = sort(distances, 'ascend');
+
+    [~,minIdx] = min(distances);
+
+    % The closest non-nan value will be located at index sortedIndexes(1)
+    %indexOfClosest = distances(minIdx);
+
+    r2 = R(minIdx);
+    c2 = C(minIdx);
+
+    newIdx = sub2ind(inSize,r2,c2);
+
+
+    % Replace the bad nan value in out with the good value in 'in'
+    out(thisLinearIndex) = in(newIdx);
 end
 
-out = u;
 
 end
