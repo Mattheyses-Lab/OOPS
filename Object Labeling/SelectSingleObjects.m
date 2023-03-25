aea6e860686a53a17b1df4b5307f3a1276f01b79
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-03-25 18:59:08 -0500

    fixed a bug where objects could not be selected if multiple images were selected in the image selection uitree

--- Object Labeling/SelectSingleObjects.m
+++ Object Labeling/SelectSingleObjects.m
@@ -6,11 +6,9 @@ function SelectSingleObjects(source,~)
     % store this handle so we can use it to update after deleting an object box
     hAx = source.Parent;
 
-    %CurrentPointFull = source.Parent.CurrentPoint(1,1);
-
     PODSData = guidata(source);
 
-    CurrentImage = PODSData.CurrentImage;
+    CurrentImage = PODSData.CurrentImage(1);
 
     x = round(CurrentPoint(1));
     y = round(CurrentPoint(2));
@@ -21,34 +19,6 @@ function SelectSingleObjects(source,~)
         return
     end
 
-% below: finding clicked object by looking for closest centroid
-%     % initialize array of object centroid coordinates
-%     Centroids = zeros(2,CurrentImage.nObjects);
-%     %CentroidsY = zeros(1,CurrentImage.nObjects);
-%     % get x-, and y-coordinates for each centroid
-%     [Centroids(1,:)] = deal([CurrentImage.Object.CentroidX]);
-%     [Centroids(2,:)] = deal([CurrentImage.Object.CentroidY]);
-% 
-%     diff_squared = zeros(size(Centroids));
-% 
-%     diff_squared(1,:) = (abs(Centroids(1,:)-CurrentPoint(1))).^2;
-% 
-%     diff_squared(2,:) = (abs(Centroids(2,:)-CurrentPoint(2))).^2;
-% 
-%     distances = zeros(1,size(diff_squared,2));
-% 
-%     distances(:) = sqrt(diff_squared(1,:)+diff_squared(2,:));
-% 
-%     ObjIdx = find(distances==min(distances));
-
-%     switch CurrentImage.Object(ObjIdx).Selected
-%         case true
-%             CurrentImage.Object(ObjIdx).Selected = false;
-%         case false
-%             CurrentImage.Object(ObjIdx).Selected = true;
-%     end
-
-    
 
     if strcmp(PODSData.Handles.fH.SelectionType,'extend')
         CurrentImage.CurrentObjectIdx = ObjIdx;
