4565cb8679ba1c41acb170bd990dcd47e9e5d51b
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-06-01 14:07:14 -0500

    No material changes

--- src/Updating/UpdateGroupTree.m
+++ src/Updating/UpdateGroupTree.m
@@ -15,11 +15,31 @@ function UpdateGroupTree(source)
             OOPSData.Handles.GroupNodes(i) = uitreenode(OOPSData.Handles.GroupTree,...
                 'Text',CurrentGroup.GroupName,...
                 'NodeData',CurrentGroup,...
-                'Icon',makeRGBColorSquare(CurrentGroup.Color,10),...
-                'ContextMenu',OOPSData.Handles.GroupContextMenu);
+                'ContextMenu',OOPSData.Handles.GroupContextMenu,...
+                'Icon',makeRGBColorSquare(CurrentGroup.Color,5));
         end
         % decide which node is selected based on OOPSData.CurrentGroup
         OOPSData.Handles.GroupTree.SelectedNodes = OOPSData.Handles.GroupTree.Children(OOPSData.CurrentGroupIndex);
+
+
+        %% update style configurations
+    
+        % testing below
+        % % remove any previously added styles
+        % removeStyle(OOPSData.Handles.GroupTree);
+        % % get the list of child nodes
+        % groupTreeNodes = OOPSData.Handles.GroupTree.Children;
+        % % add styles for each node
+        % for nodeIdx = 1:numel(groupTreeNodes)
+        %     % get the next node
+        %     thisNode = groupTreeNodes(nodeIdx);
+        %     % define its style
+        %     thisNodeStyle = uistyle('BackgroundColor',thisNode.NodeData.Color,'FontColor',getBWContrastColor(thisNode.NodeData.Color));
+        %     % add the style to the node
+        %     addStyle(OOPSData.Handles.GroupTree,thisNodeStyle,"node",groupTreeNodes(nodeIdx));
+        % end
+        % end testing
+
     else
         % make sure tree contains no nodes if we have no groups
         delete(OOPSData.Handles.GroupTree.Children);
