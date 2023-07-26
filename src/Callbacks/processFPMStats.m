6d64aa026f9f7ae16affb1172d9b26a95acef145
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-07-26 18:39:23 -0500

    added progress dialog bars

--- src/Callbacks/processFPMStats.m
+++ src/Callbacks/processFPMStats.m
@@ -1,30 +1,38 @@
 function processFPMStats(source,~)
+
     % get handle to the data structure
     OOPSData = guidata(source);
     
     % number of selected images
-    nImages = length(OOPSData.CurrentImage);
+    nImages = numel(OOPSData.CurrentImage);
+
     % update log to indicate # of images we are processing
     UpdateLog3(source,['Computing order statistics statistics for ',num2str(nImages),' images'],'append');
-    % counter to track progress
-    Counter = 1;
+
     % start a timer
     tic
+
+    % testing below - progress dialog
+    hProgressDialog = uiprogressdlg(OOPSData.Handles.fH,"Message",'Calculating order and orientation statistics');
+
     % compute pixel-by-pixel FPM stats for each selected image
-    for cImage = OOPSData.CurrentImage
+    for i = 1:nImages
+        cImage = OOPSData.CurrentImage(i);
+        % update the progress dialog
+        hProgressDialog.Message = ['Calculating order and orientation statistics ',num2str(i),'/',num2str(nImages)];
+        hProgressDialog.Value = i/nImages;
         % update log to indicate which image we are on
-        UpdateLog3(source,['    ',cImage.rawFPMShortName,' (',num2str(Counter),'/',num2str(nImages),')'],'append');
+        UpdateLog3(source,['    ',cImage.rawFPMShortName,' (',num2str(i),'/',num2str(nImages),')'],'append');
         % compute the FPM stats
         cImage.FindOrderFactor();
     
         % testing below - additional FPM stats
         cImage.ComputeFPMStats();
         % end testing
-        % increment the counter
-        Counter = Counter+1;
     end
     % end the timer and save the time
     timeElapsed = toc;
+
     % change to the Order Factor 'tab' if not there already
     if ~strcmp(OOPSData.Settings.CurrentTab,'Order Factor')
         feval(OOPSData.Handles.hTabOrderFactor.Callback,OOPSData.Handles.hTabOrderFactor,[]);
@@ -32,10 +40,16 @@ function processFPMStats(source,~)
         % update displayed images (tab switching will automatically update the display)
         UpdateImages(source);
     end
+
     % update summary table
     UpdateSummaryDisplay(source,{'Group','Image','Object'});
+    
     % update log with time elapsed
     UpdateLog3(source,['Time elapsed: ',num2str(timeElapsed),' seconds'],'append');
+
+    % close the progress dialog
+    close(hProgressDialog);
+
     % update log to indicate we are done
     UpdateLog3(source,'Done.','append');
 
--- src/Callbacks/processLocalSB.m
+++ src/Callbacks/processLocalSB.m
@@ -1,26 +1,37 @@
 function processLocalSB(source,~)
+
     % get handle to the data structure
     OOPSData = guidata(source);
 
     % number of selected images
-    nImages = length(OOPSData.CurrentImage);
+    nImages = numel(OOPSData.CurrentImage);
+
     % update log to indicate # of images we are processing
     UpdateLog3(source,['Detecting Local S/B for ',num2str(nImages),' images'],'append');
-    % counter to track which image we're on
-    Counter = 1;
-    for cImage = OOPSData.CurrentImage
+
+    % create progress dialog
+    hProgressDialog = uiprogressdlg(OOPSData.Handles.fH,"Message",'Calculating local S/B');
+
+    for i = 1:nImages
+        cImage = OOPSData.CurrentImage(i);
+        % update the progress dialog
+        hProgressDialog.Message = ['Calculating local S/B ',num2str(i),'/',num2str(nImages)];
+        hProgressDialog.Value = i/nImages;
         % update log to indicate which image we are on
-        UpdateLog3(source,['    ',cImage.rawFPMShortName,' (',num2str(Counter),'/',num2str(nImages),')'],'append');
+        UpdateLog3(source,['    ',cImage.rawFPMShortName,' (',num2str(i),'/',num2str(nImages),')'],'append');
         % detect local S/B for one image
         cImage.FindLocalSB();
         % log update to indicate we are done with this image
         UpdateLog3(source,['        Local S/B detected for ',num2str(cImage.nObjects),' objects...'],'append');
-        % increment counter
-        Counter = Counter+1;
     end
-    % update log to indicate we are done
-    UpdateLog3(source,'Done.','append');
+
     % update summary table
     UpdateSummaryDisplay(source,{'Group','Image','Object'});
+
+    % close the progress dialog
+    close(hProgressDialog);
+
+    % update log to indicate we are done
+    UpdateLog3(source,'Done.','append');    
 
 end
