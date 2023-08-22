34aa6bdbfc0090d16c657db72c7fe0335f592b67
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-08-22 02:35:10 -0500

    Improved file loading: support for more formats, progress bars, more consistent variable names, bettor error checking

--- src/Callbacks/loadFPMImages.m
+++ src/Callbacks/loadFPMImages.m
@@ -8,7 +8,8 @@ function loadFPMImages(source,~)
     cGroup = OOPSData.Group(GroupIndex);
     
     % alert box to indicate required action, closing will resume interaction on main window
-    uialert(OOPSData.Handles.fH,'Select .nd2 or .tif polarization stack(s)',...
+    uialert(OOPSData.Handles.fH,...
+        'Select FPM stack(s)',...
         'Load FPM Data',...
         'Icon','',...
         'CloseFcn',@(o,e) uiresume(OOPSData.Handles.fH));
@@ -17,15 +18,24 @@ function loadFPMImages(source,~)
     % hide main window
     OOPSData.Handles.fH.Visible = 'Off';
 
+    % load the Bio-Formats library into the MATLAB environment
+    % so we can call uigetfile with filters for all extensions
+    % supported by Bio-Formats
+    if bfCheckJavaPath()
+        fileExtensions = bfGetFileExtensions();
+    else
+        fileExtensions = {'*'};
+    end
+
     % try to get files from the most recent directory, otherwise just use default
     try
-        [FPMFiles, FPMPath, ~] = uigetfile({'*.nd2;*.tif'},...
-            'Select .nd2 or .tif polarization stack(s)',...
+        [FPMFiles, FPMPath, ~] = uigetfile(fileExtensions,...
+            'Select FPM stack(s)',...
             'MultiSelect','on',...
             OOPSData.Settings.LastDirectory);
     catch
-        [FPMFiles, FPMPath, ~] = uigetfile({'*.nd2;*.tif'},...
-            'Select .nd2 or .tif polarization stack(s)',...
+        [FPMFiles, FPMPath, ~] = uigetfile(fileExtensions,...
+            'Select FPM stack(s)',...
             'MultiSelect','on');
     end
 
@@ -37,12 +47,9 @@ function loadFPMImages(source,~)
     figure(OOPSData.Handles.fH);
 
     if ~iscell(FPMFiles)
-        % if no files selected
-        if FPMFiles == 0
-            % throw error
-            error('No files selected');
-        else
-            % otherwise convert to cell array
+        if FPMFiles == 0 % if no files selected
+            uialert(OOPSData.Handles.fH,'No files selected','Error'); return
+        else % convert to cell array
             FPMFiles = {FPMFiles};
         end
     end
@@ -51,17 +58,17 @@ function loadFPMImages(source,~)
     nFiles = numel(FPMFiles);
 
     % update log
-    UpdateLog3(source,['Opening ',num2str(nFiles),' FPM images...'],'append');
+    UpdateLog3(source,['Opening ',num2str(nFiles),' FPM stacks...'],'append');
 
-    % testing below - progress dialog
+    % create progress dialog
     hProgressDialog = uiprogressdlg(OOPSData.Handles.fH,"Message",'Loading FPM stacks');
 
     % for each file (stack of 4 polarization images)
     for i=1:nFiles
 
+        % update the progress dialog
         hProgressDialog.Message = ['Loading FPM stacks ',num2str(i),'/',num2str(nFiles)];
         hProgressDialog.Value = i/nFiles;
-
 
         % get the name of this file
         rawFPMFileName = FPMFiles{1,i};
@@ -82,11 +89,11 @@ function loadFPMImages(source,~)
         % make sure this is a 4-image stack
         nSlices = length(imageInfo(:,1));
 
-        if nSlices ~= 4
-            error('LoadFPMData:incorrectSize', ...
-                ['Error while loading ', ...
-                rawFPMFullName, ...
-                '\nFile must be a stack of four images'])
+        % check for correct input dimensions
+        try
+            assert(nSlices==4,'Error while loading %s\nFile must be a stack of four images',rawFPMFullName)
+        catch ME
+            uialert(OOPSData.Handles.fH,ME.message,'Error'); return
         end
 
         % from the bfdata cell array of image data, concatenate slices along 3rd dim and convert to matrix
@@ -111,7 +118,26 @@ function loadFPMImages(source,~)
         % store the handle to the new OOPSImage in a separate variable for readability below
         cImage = cGroup.Replicate(end);
 
-        % get the name of this file
+
+        % determine the height (number of rows) of the input data
+        Height = size(rawFPMStack,1);
+        % determine the width (number of rows) of the input data
+        Width = size(rawFPMStack,2);
+
+        % ensure all image dimensions match those of the first image in the group
+        if cImage.SelfIdx ~= 1
+            try
+                assert(isequal([Height,Width],size(rawFPMStack,[1 2])),'All FPM images must have the same dimensions');
+            catch ME
+                uialert(OOPSData.Handles.fH,ME.message,'Error'); return
+            end
+        end
+
+        % store the Height
+        cImage.Height = Height;
+        % store the Width
+        cImage.Width = Width;
+        % store the name of this file (without path, with extension)
         cImage.rawFPMFileName = rawFPMFileName;
         % store the 'short' filename (without path and extension)
         cImage.rawFPMShortName = rawFPMShortName;
@@ -125,10 +151,6 @@ function loadFPMImages(source,~)
         cImage.rawFPMClass = rawFPMClass;
         % get the range of values in the input stack using its class
         cImage.rawFPMRange = rawFPMRange;
-        % determine the height (number of rows) of the input data
-        cImage.Height = size(rawFPMStack,1);
-        % determine the width (number of rows) of the input data
-        cImage.Width = size(rawFPMStack,2);
         % add the raw image data to this OOPSImage
         cImage.rawFPMStack = rawFPMStack;
 
@@ -151,9 +173,9 @@ function loadFPMImages(source,~)
 
         % update log to display image dimensions
         UpdateLog3(source,['Dimensions of ', ...
-            char(cImage.rawFPMShortName), ...
+            char(cImage.rawFPMFileName), ...
             ' are ', num2str(cImage.Width), ...
-            ' by ', num2str(cImage.Height)], ...
+            'x', num2str(cImage.Height)], ...
             'append');
     end
 
