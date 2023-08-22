c939fcffead878a492b58894b093481a48e4aa2b
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-08-22 03:58:49 -0500

    edited header

--- src/Dependencies/traceFromEndpoint.m
+++ src/Dependencies/traceFromEndpoint.m
@@ -1,33 +1,24 @@
 function tracedCurve = traceFromEndpoint(point,remainingPoints)
-%-------------------------------------------------------------------------------------%
-% FUNCTION NAME:
-%   traceFromEndpoint
+% traceFromEndpoint  Given a list of unsorted points representing an unclosed 
+% curve and one endpoint of the curve, recursively reorder the points by finding the 
+% shortest path through them.
 %
-% DESCRIPTION:
-%   Given a list of unsorted points representing an unclosed curve and one 
-%   endpoint of the curve, recursively reorder the points by finding the 
-%   shortest path through them.
+%   INPUTS:
+%       point (1x2 double) - (x,y) coordinates of one endpoint of the curve
+%       remainingPoints (mx2 double) - (x,y) coordinates of the remaining points in the curve
 %
-% INPUT:
-%   point - (1x2 double) (x,y) coordinates of one endpoint of the curve
-%   remainingPoints - (mx2 double) (x,y) coordinates of the remaining points in the curve
+%   OUTPUTS:
+%       out (m+1x2 double) - reordered (x,y) coordinates, including the endpoint, point
 %
-% OUTPUT:
-%   out - (m+1x2 double) reordered (x,y) coordinates, including the endpoint, point
-%
-% ASSUMPTIONS AND LIMITATIONS:
-%   point must be an endpoint of the curve
-%   potential to fail if distances between endpoints and all other points is not greater 
-%       than or equal to the largest distance between adjacent points
-%   potential to fail if the curve has sharp turns, especially near the endpoints
-%   will not work properly if the curve intersects itself
-%   similarly, will give strange output if the curve has any branches (i.e., > 2 end points)
-%
-% REVISION HISTORY:
-%   12/22/2022 - Will Dean
-%       * Initial implementation
-%
-%-------------------------------------------------------------------------------------%
+%   ASSUMPTIONS AND LIMITATIONS:
+%       point must be an endpoint of the curve
+%       potential to fail if distances between endpoints and all other points is not greater 
+%           than or equal to the largest distance between adjacent points
+%       potential to fail if the curve has sharp turns, especially near the endpoints
+%       will not work properly if the curve intersects itself
+%       similarly, will give strange output if the curve has any branches (i.e., > 2 end points)
+
+
 % if only one point left, add it to the end of the list and return
 if size(remainingPoints,1)==1
     tracedCurve = [point;remainingPoints];
