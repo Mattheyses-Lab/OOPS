9979f9a9ae332d7594b3a97892ad118b48ebc3d5
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-10-01 17:44:15 -0500

    renamed variables and removed a loop to increase efficiency and improve readability

--- src/Plotting/PlotObjectIntensityProfile.m
+++ src/Plotting/PlotObjectIntensityProfile.m
@@ -1,6 +1,6 @@
 function axH = PlotObjectIntensityProfile(...
-    x,...
-    Y,...
+    xReal,...
+    yReal,...
     Mask,...
     axH,...
     FitLineColor,...
@@ -13,24 +13,23 @@ function axH = PlotObjectIntensityProfile(...
     
     nPixels = length(RowIndices);
     
-    xp = linspace(0,pi,181);
+    xFit = linspace(0,pi,181);
 
-    CurveFit = cell(1,nPixels);
+    % prealocate cell array of sinusoidal fits for each pixel
+    CurveFit = cell(nPixels,1);
 
     parfor i = 1:nPixels
         
-        y = zeros(1,4);
-        y(1:4) = Y(RowIndices(i),ColIndices(i),:);
+        yFit = zeros(1,4);
+        yFit(1:4) = yReal(RowIndices(i),ColIndices(i),:);
         
 %% Estimate initial values
-        yu = max(y);
-        yl = min(y);
-        yr = (yu-yl);                               % Range of ‘y’
-        yz = y-yu+(yr/2);
-        zx = x(yz .* circshift(yz,[0 1]) <= 0);     % Find zero-crossings
+        yMax = max(yFit);
+        yMin = min(yFit);
+        yRange = (yMax-yMin);
 
         % Estimate offset
-        ym = mean(y);
+        yMean = mean(yFit);
 
 %% Anonymous fitting functions        
         % Function to fit
@@ -45,65 +44,54 @@ function axH = PlotObjectIntensityProfile(...
         % but the fit parameters do not match those from a generic cos^2 
         fit = @(b,x)  b(1) .* ((1 + cos(2.*(x-b(2))))./2) + b(3);
 
-        % Least-Squares cost function
-        fcn = @(b) sum((fit(b,x) - y).^2);
+        % Least-Squares cost function to minimize
+        fcn = @(b) sum((fit(b,xReal) - yFit).^2);
+
         % Minimise Least-Squares
-        s = fminsearch(fcn, [yr; 1;  ym]);
+        s = fminsearch(fcn, [yRange; 1;  yMin]);
+
 %% Get y values of curve fit for current pixel       
 
         % y values to plot - found with the fitting function
-        CurveFit{i} = fit(s,xp);
+        CurveFit{i} = fit(s,xFit);
 
     end
 
 
-    CurveFitsNormalized = CurveFit;
+%% concatenate fit curves and phase lines for all pixels (new method without loop)
 
+    % convert CurveFit cell array to nPixels x 181 matrix
+    CurveFitMat = cell2mat(CurveFit);
+    % get the average of all fits
+    CurveFitAvg = mean(CurveFitMat,1);
+    % create a cell array of NaNs, same size as CurveFit
+    nanCell = arrayfun(@(x) x,nan(nPixels,1),'UniformOutput',false);
+    % cell array with nPixels copies of xFit vectors
+    xFitCell = repmat({xFit},nPixels,1);
+    % combined x and y data for all fit lines (excluding average fit)
+    xFitCombined = cell2mat(Interleave2DArrays(xFitCell,nanCell,'row')');
+    yFitCombined = cell2mat(Interleave2DArrays(CurveFit,nanCell,'row')');
+    % get max values and idx to the max values for each fit
+    [maxVal,maxIdx] = max(CurveFitMat,[],2);
+    % combined x and y data for all vertical phase lines
+    xMaxCombined = cell2mat(Interleave2DArrays(arrayfun(@(x) [x x],xFit(maxIdx)','UniformOutput',false),nanCell,'row')');
+    yMaxCombined = cell2mat(Interleave2DArrays(arrayfun(@(x) [0 x],maxVal,'UniformOutput',false),nanCell,'row')');
+    % get max and minimum values for all curve fits
+    MaxY = max(CurveFitMat,[],"all");
+    MinY = min(CurveFitMat,[],"all");
+    % plot each fit curve along with vertical lines to show phase
+    line(axH,xMaxCombined,yMaxCombined,'Color',AzimuthLinesColor,'LineStyle',':','LineWidth',1,'HitTest','Off');
+    line(axH,xFitCombined,yFitCombined,'Color',PixelLinesColor,'LineWidth',1,'HitTest','Off','LineStyle',':');
     
-    % initialize vector to sum up fits
-    CurveFitSum = zeros(size(CurveFit{1}));
-
-    fH = ancestor(axH,'figure');
-    set(fH,'CurrentAxes',axH);
-    
-
-    polX_combined = [];
-    polY_combined = [];
-
-    azX_combined = [];
-    azY_combined = [];
-
-    for i = 1:nPixels
-        CurveFitSum = CurveFitSum+CurveFitsNormalized{i};
-        % plot the fit line
-        polX_combined = [polX_combined,NaN,xp];
-        polY_combined = [polY_combined,NaN,CurveFitsNormalized{i}];
-        % find the max of each curve and draw a vertical line at x where f(x) = YMax
-        % to show the phase
-        MaxVal = max(CurveFitsNormalized{i});
-        % 2nd argument of find() set to 1 just in case MaxIdx is exactly
-        % 0, which would make make MaxIdx also = pi
-        MaxIdx = find(CurveFitsNormalized{i} == MaxVal,1);
-        azX_combined = [azX_combined,NaN,xp(MaxIdx),xp(MaxIdx)];
-        azY_combined = [azY_combined,NaN,0,MaxVal];
-        %line(axH,[xp(MaxIdx),xp(MaxIdx)],[0,MaxVal],'LineStyle',':','LineWidth',1,'HitTest','Off');
-    end
-    line(axH,azX_combined,azY_combined,'Color',AzimuthLinesColor,'LineStyle',':','LineWidth',1,'HitTest','Off');
-    line(axH,polX_combined,polY_combined,'Color',PixelLinesColor,'LineWidth',1,'HitTest','Off','LineStyle',':')
-
-    CurveFitAvg = CurveFitSum./nPixels;
-%%
-
-    MaxY = max(cell2mat(CurveFitsNormalized));
-    MinY = min(cell2mat(CurveFitsNormalized));
-    
+    % amount of buffer on either side of the fit lines (in y)
     PctBuffer = 0.1;
     
+    % upper and lower y-axes limits
     YLowerLim = MinY-PctBuffer;
     YUpperLim = MaxY+PctBuffer;
 
     % plot the average fit line
-    line(axH,xp,CurveFitAvg,'LineWidth',4,'Color',FitLineColor);
+    line(axH,xFit,CurveFitAvg,'LineWidth',4,'Color',FitLineColor);
     
     MaxVal = max(CurveFitAvg);
     MaxIdx = find(CurveFitAvg==MaxVal,1);
@@ -111,52 +99,43 @@ function axH = PlotObjectIntensityProfile(...
     MinVal = min(CurveFitAvg);
     MinIdx = find(CurveFitAvg==MinVal,1);
     
-    OrderFactorFit = MaxVal-MinVal;
-    AzimuthRadians = xp(MaxIdx);
+    OrderFit = MaxVal-MinVal;
+    AzimuthRadians = xFit(MaxIdx);
     
-    % dotted vert line showing azimuth
-    line(axH,[xp(MaxIdx),xp(MaxIdx)],[0,MaxVal],'LineStyle','--','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
-    
-    % dashed line showing max value
-    line(axH,[xp(MaxIdx),xp(MinIdx)],[MaxVal,MaxVal],'LineStyle','--','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
-    
-    % vert line and text object showing OF fit
-    line(axH,[xp(MinIdx),xp(MinIdx)],[MaxVal,MinVal],'LineStyle','-','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
-    txtOF = [' Order (fit): ',num2str(round(OrderFactorFit,2)),' '];
+    % dotted vertical line showing azimuth
+    line(axH,[xFit(MaxIdx),xFit(MaxIdx)],[0,MaxVal],'LineStyle','--','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
+    % dashed horizontal line showing max value
+    line(axH,[xFit(MaxIdx),xFit(MinIdx)],[MaxVal,MaxVal],'LineStyle','--','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
+    % vertical line at minimum shwoing peak-to-peak amplitude
+    line(axH,[xFit(MinIdx),xFit(MinIdx)],[MaxVal,MinVal],'LineStyle','-','LineWidth',1,'HitTest','Off','Color',AnnotationsColor);
+
+    % text for amplitude and phase labels
+    txtOrder = [' Order (fit): ',num2str(round(OrderFit,2)),' '];
     txtAzimuth = [' Azimuth (fit): ',num2str(round(rad2deg(AzimuthRadians))),'° '];
     
-    if xp(MaxIdx) > pi/2
-        text(axH,...
-            xp(MinIdx),MaxVal-OrderFactorFit/2,...
-            txtOF,...
-            'Units','data',...
-            'HorizontalAlignment','Left',...
-            'Color',AnnotationsColor);
-        text(axH,...
-            xp(MaxIdx),MaxVal-(MaxVal-YLowerLim)/2,...
-            txtAzimuth,...
-            'Units','data',...            
-            'HorizontalAlignment','Right',...
-            'Color',AnnotationsColor);
+    % determine whether to align text left or right
+    if xFit(MaxIdx) > pi/2
+        orderTextAlignment = 'Left';
+        azimuthTextAlignment = 'Right';
     else
-        text(axH,...
-            xp(MinIdx),MaxVal-OrderFactorFit/2,...
-            txtOF,...
-            'Units','data',...            
-            'HorizontalAlignment','Right',...
-            'Color',AnnotationsColor);
-        text(axH,...
-            xp(MaxIdx),MaxVal-(MaxVal-YLowerLim)/2,...
-            txtAzimuth,...
-            'Units','data',...            
-            'HorizontalAlignment','Left',...
-            'Color',AnnotationsColor);
+        orderTextAlignment = 'Right';
+        azimuthTextAlignment = 'Left'; 
     end
-    
-    hold off
-    
-    %set(gcf,'Position',[1,1,1792,400]);
-    set(axH,'XLim',[0 pi]);
-    set(axH,'YLim',[YLowerLim YUpperLim]);
+
+    text(axH,...
+        xFit(MinIdx),MaxVal-OrderFit/2,...
+        txtOrder,...
+        'Units','data',...
+        'HorizontalAlignment',orderTextAlignment,...
+        'Color',AnnotationsColor);
+    text(axH,...
+        xFit(MaxIdx),MaxVal-(MaxVal-YLowerLim)/2,...
+        txtAzimuth,...
+        'Units','data',...
+        'HorizontalAlignment',azimuthTextAlignment,...
+        'Color',AnnotationsColor);
+
+    % set axes limits
+    set(axH,'XLim',[0 pi],'YLim',[YLowerLim YUpperLim]);
 
 end
