3868b6eb14327198dc02634d74c1dbc264504471
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-10-19 09:32:45 -0500

    Adjusted the function to return one additional line

--- src/Dependencies/MultiParallelTranslation.m
+++ src/Dependencies/MultiParallelTranslation.m
@@ -1,10 +1,11 @@
 function ParallelLines = MultiParallelTranslation(x1,x2,y1,y2,n)
-% Returns a nx1 cell array where each cell is a 2x2 double containing the coordinates
-% of of a line parallel to the line formed by points (x1,y1),(x2,y2)
+% Returns an (n+1,1) cell array where each cell is a 2x2 double containing the coordinates
+% of either the original line (first cell | (x1,y1),(x2,y2)) or a line parallel to it
+% n is the number of lines created from the first line
 
     ParallelLines{1,1} = [x1,y1;x2,y2];
 
-    for i = 2:n
+    for i = 2:n+1
 
         [x1,x2,y1,y2] = ParallelTranslation(x1,x2,y1,y2,1);
 
