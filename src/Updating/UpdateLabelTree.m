c3bae30eca1eb4b4872c9f3050ee19c749be09fc
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-11-06 13:21:31 -0600

    deleted comments

--- src/Updating/UpdateLabelTree.m
+++ src/Updating/UpdateLabelTree.m
@@ -1,10 +1,12 @@
 function UpdateLabelTree(source)
 
+    % the main data structure
     OOPSData = guidata(source);
 
-    % delete previous label tree nodes
+    % delete existing label tree nodes
     delete(OOPSData.Handles.LabelTree.Children);
     
+    % build new nodes
     for i = 1:numel(OOPSData.Settings.ObjectLabels)
         uitreenode(OOPSData.Handles.LabelTree,...
             'Text',OOPSData.Settings.ObjectLabels(i).Name,...
@@ -12,23 +14,5 @@ function UpdateLabelTree(source)
             'ContextMenu',OOPSData.Handles.LabelContextMenu,...
             'Icon',makeRGBColorSquare(OOPSData.Settings.ObjectLabels(i).Color,5));
     end
-
-    %% update style configurations
-
-    % testing below
-    % % remove any previously added styles
-    % removeStyle(OOPSData.Handles.LabelTree);
-    % % get the list of child nodes
-    % labelTreeNodes = OOPSData.Handles.LabelTree.Children;
-    % % add styles for each node
-    % for nodeIdx = 1:numel(labelTreeNodes)
-    %     % get the next node
-    %     thisNode = labelTreeNodes(nodeIdx);
-    %     % define its style
-    %     thisNodeStyle = uistyle('BackgroundColor',thisNode.NodeData.Color,'FontColor',getBWContrastColor(thisNode.NodeData.Color));
-    %     % add the style to the node
-    %     addStyle(OOPSData.Handles.LabelTree,thisNodeStyle,"node",labelTreeNodes(nodeIdx));
-    % end
-    % end testing
 
 end
