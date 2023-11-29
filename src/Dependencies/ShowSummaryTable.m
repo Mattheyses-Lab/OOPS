f442f3678b2c0147a7147056378f09d24c299249
will-f-dean-36 <will.f.dean.36@gmail.com>
2023-11-29 03:57:31 -0600

    fixed issue where summary window was not full screen, reordered variables, and use more readable column names

--- src/Dependencies/ShowSummaryTable.m
+++ src/Dependencies/ShowSummaryTable.m
@@ -20,19 +20,21 @@ function ShowSummaryTable(source,~)
 %
 %----------------------------------------------------------------------------------------------------------------------------
 
-    % get the summary table
-    T = SaveOOPSData(source);
-    
-    % handle to the main data structure
-    OOPSData = guidata(source);
+% handle to the main data structure
+OOPSData = guidata(source);
 
-    % new figure to show summary table
-    SummaryFig = uifigure('Position',OOPSData.Handles.fH.Position);
-    
-    % uitable to hold data
-    uit = uitable(SummaryFig,'data',T);
+% get the stacked data tables and concatenate them
+stackedDataTables = OOPSData.stackedObjectDataTable();
+T = cat(1,stackedDataTables(:).Data);
 
-    % make uitable fill the figure
-    uit.Units = 'normalized';
-    uit.Position = [0 0 1 1];
+% new figure to show summary table
+SummaryFig = uifigure('Position',OOPSData.Settings.ScreenSize);
+
+% uitable to hold data
+uit = uitable(SummaryFig,'data',T);
+
+% make uitable fill the figure
+uit.Units = 'normalized';
+uit.Position = [0 0 1 1];
+
 end
